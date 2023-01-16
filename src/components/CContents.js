6a357108bcbb36a2cd3a077334e3feb1609fb8e5
Mykhailo Kulyk <programist256@gmail.com>
2023-01-16 19:23:22 +0100

    may_be_end

--- src/components/CContents.js
+++ src/components/CContents.js
@@ -1,17 +1,31 @@
 import React, {Component} from "react";
+import './CContent.css'
+import jsonData from '../res/text.json'
 
 class CContents extends Component{
     constructor(){
         super()
         this.state = {msg: 'Witaj w React, Klikni tutaj dla wiadomosci'}
+        this.txtData = () => JSON.parse(JSON.stringify(jsonData));
     }
 
     changeMsg(){
         this.setState({msg: 'Jestes uczestnikem React'});
     }
     render(){
+        const item = [];
+        for(let i = 0; i<this.txtData().count; i++){
+            let value = this.txtData().text[i];
+            item.push(
+                <div id={'"div${i+1}"'} className="contentsFlex">
+                    <img className="contentsFlexImg" src={'/img/img'+(i+1)+'.png'} alt=""/>
+                    <p className="contentsFlexTxt">{value}</p>
+                </div>
+            )
+        }
         return(
-            <div>
+            <div className="contentsDivClass">
+                {item}
                 <h1>{this.state.msg}</h1>
                 <button onClick = {()=>this.changeMsg()}>
                     Kliknij, aby poznac
