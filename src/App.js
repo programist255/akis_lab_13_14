09f95d61ce182a8de07107742b5de6549dd22ccc
Mykhailo Kulyk <programist256@gmail.com>
2023-01-16 18:59:33 +0100

    App_new_cont

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import {CFooter} from './components/CFooter';
 import CHeader from './components/CHeader';
+import CContents from './components/CContents';
 
 function App() {
   return (
     <div className="App">
-      <CHeader aName='Mykhailo' aFamily='Kulyk'/>
-      <CFooter weather = 'slonce' temp='25'/>
+      {/* <CHeader aName='Mykhailo' aFamily='Kulyk'/> */}
+      {/* <CFooter weather = 'slonce' temp='25'/>
       <CFooter weather = 'deszczowa' temp='13'/>
       <CFooter weather = 'sniezna' temp='-8'/>
       <CFooter weather = 'slonce' temp='20'>
         <p>Niebo jest w chmurze, wiatru nie ma</p>
-      </CFooter>
-      
+      </CFooter> */}
+      <CContents/>
     </div>
   );
 }
